a2f62dc28207807e8b1f6f4c70750789a32df620
HEXHUB <Neliogaultier@gmail.com>
2025-03-02 19:22:35 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,22 +1,4 @@
-
-function base64decode(data)
-    local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-    local t = {}
-    local result = ""
-    data = string.gsub(data, "[^" .. b .. "=]", "")
-    for i = 1, #data, 4 do
-        local c = string.sub(data, i, i + 3)
-        c = string.gsub(c, "=", "A")
-        local n = 0
-        for j = 1, 4 do
-            local k = string.find(b, string.sub(c, j, j)) - 1
-            n = n * 64 + k
-        end
-        for j = 3, 1, -1 do
-            local k = math.floor(n / (256 ^ (j - 1))) % 256
-            result = result .. string.char(k)
-        end
-    end
-    return result
-end
-loadstring(base64decode("cHJpbnQoIjEzNDE0NTY0NDk1MjM2NzkyOTMgaXMgQHJlYnJhbmRlZHNraWRgcyBEaXNjb3JkSUQgR08gUkFJRCBISU1NTSIp"))()
+local vm={stack={},instructions={},ip=1}function vm:push(value)table.insert(self.stack,value)end function vm:pop()return table.remove(self.stack)end function vm:run()while self.ip<=#self.instructions do local inst=self.instructions[self.ip]if inst.op=="PUSH"then self:push(inst.value)elseif inst.op=="CALL"then local func=_G[inst.func]if func then local args={}for i=1,inst.arg_count do table.insert(args,1,self:pop())end func(unpack(args))end end self.ip=self.ip+1 end end
+local bytecode={{op="PUSH",value="hello"},{op="CALL",func="print",arg_count=1}}
+vm.instructions=bytecode
+vm:run()
