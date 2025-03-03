ec811c5dad30a371835938abc73c1552e933b2b4
HEXHUB <Neliogaultier@gmail.com>
2025-03-03 22:44:54 +0100

    Update TestNew.lua

--- TestNew.lua
+++ TestNew.lua
@@ -1,11 +1,35 @@
-local vm={stack={},instructions={},ip=1,vars={}}
-local output_log=""
-local identityFunc=function()end
-identityFunc("Wave Executor running")
+local vm={stack={},r={},ip=1,constants={},code={},vars={}}local output_log=""
+local tostring=tostring or function(v)return v end
+local pcall=pcall or function()end
+local unpack=unpack or function()end
+local print=print or function()end
+local warn=warn or function()end
+local getmetatable=getmetatable or function()end
+local setmetatable=setmetatable or function()end
+local type=type or function()return"unknown"end
 function vm:log(msg)print(msg)output_log=output_log..msg.."
 "end
-function vm:run()while self.ip<=#self.instructions do self.ip=self.ip+1 end end
-vm.constants={}
-vm.code={}
-
+function vm:push(value)self:log("PUSH: "..tostring(value))table.insert(self.stack,value)end
+function vm:pop()local v=table.remove(self.stack)self:log("POP: "..tostring(v))return v end
+function vm:get_reg(reg)self:log("GET_REG: r["..reg.."]="..tostring(self.r[reg]))return self.r[reg]end
+function vm:set_reg(reg,value)self:log("SET_REG: r["..reg.."]="..tostring(value))self.r[reg]=value end
+function vm:run()self:log("LuaRPH-like VM Starting")while self.ip<=#self.code do local inst=self.code[self.ip]self:log("Executing: "..inst.op.." at ip: "..self.ip)
+if inst.op=="LOADK"then local reg=inst.reg local k=inst.k self:set_reg(reg,self.constants[k])self.ip=self.ip+1
+elseif inst.op=="MOVE"then local src=inst.src local dst=inst.dst local val=self:get_reg(src)self:set_reg(dst,val)self.ip=self.ip+1
+elseif inst.op=="GETVAR"then local value=self.vars[inst.var_name] or 0 self:push(value)self.ip=self.ip+1
+elseif inst.op=="SETVAR"then local value=self:pop()self.vars[inst.var_name]=value self.ip=self.ip+1
+elseif inst.op=="ADD"then local a=inst.a local b=inst.b local c=inst.c local val1=self:get_reg(b)local val2=self:get_reg(c)local result=val1+val2 self:set_reg(a,result)self.ip=self.ip+1
+elseif inst.op=="SUB"then local a=inst.a local b=inst.b local c=inst.c local val1=self:get_reg(b)local val2=self:get_reg(c)local result=val1-val2 self:set_reg(a,result)self.ip=self.ip+1
+elseif inst.op=="MUL"then local a=inst.a local b=inst.b local c=inst.c local val1=self:get_reg(b)local val2=self:get_reg(c)local result=val1*val2 self:set_reg(a,result)self.ip=self.ip+1
+elseif inst.op=="DIV"then local a=inst.a local b=inst.b local c=inst.c local val1=self:get_reg(b)local val2=self:get_reg(c)local result=val1/val2 self:set_reg(a,result)self.ip=self.ip+1
+elseif inst.op=="CMP"then local b=self:pop()local a=self:pop()local result=0 if inst.cmp_type=="EQ"then result=(a==b) and 1 or 0 elseif inst.cmp_type=="LT"then result=(a<b) and 1 or 0 elseif inst.cmp_type=="GT"then result=(a>b) and 1 or 0 end self:push(result)self.ip=self.ip+1
+elseif inst.op=="JMP"then self:log("JMP: offset: "..inst.offset) self.ip=self.ip+inst.offset self:log("Jumping to ip: "..self.ip)
+elseif inst.op=="JMPZ"then local condition=self:pop()self:log("JMPZ: condition: "..tostring(condition)..", offset: "..inst.offset) if condition==0 then self.ip=self.ip+inst.offset self:log("Jumping to ip: "..self.ip)else self.ip=self.ip+1 end
+elseif inst.op=="CALL"then if inst.func=="print" then local args={}for i=1,inst.arg_count do table.insert(args,1,self:pop())end self:log("Calling print with args: "..table.concat(args,","))print(unpack(args))else self:log("Unsupported function: "..inst.func)end self.ip=self.ip+1
+elseif inst.op=="NEWTABLE"then local reg=inst.reg self:set_reg(reg,{})self.ip=self.ip+1
+elseif inst.op=="SETTABLE"then local value=self:get_reg(inst.value_reg) local key=self:get_reg(inst.key_reg) local table=self:get_reg(inst.table_reg) table[key]=value self.ip=self.ip+1
+elseif inst.op=="GETTABLE"then local table=self:get_reg(inst.table_reg) local key=self:get_reg(inst.key_reg) local value=table[key] self:set_reg(inst.dst_reg,value)self.ip=self.ip+1
+else self:log("Unknown opcode: "..inst.op) self.ip=self.ip+1 end end self:log("LuaRPH-like VM Finished")if setclipboard then local success,err=pcall(setclipboard,output_log) if not success then print("Failed to copy to clipboard: "..tostring(err)) end else print("setclipboard not supported in this environment") end end
+vm.constants={"hello",5,3,10,4,6,2,15,"x is greater than 5","x is not greater than 5",8,"x equals 8","x does not equal 8","x is less than 10","x is not less than 10"}
+vm.code={{op="LOADK",reg=0,k=0},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=1,k=1},{op="LOADK",reg=2,k=2},{op="ADD",a=3,b=1,c=2},{op="SETVAR",var_name="x"},{op="LOADK",reg=4,k=3},{op="LOADK",reg=5,k=4},{op="SUB",a=6,b=4,c=5},{op="SETVAR",var_name="y"},{op="LOADK",reg=7,k=5},{op="LOADK",reg=8,k=6},{op="MUL",a=9,b=7,c=8},{op="SETVAR",var_name="z"},{op="LOADK",reg=10,k=7},{op="LOADK",reg=11,k=2},{op="DIV",a=12,b=10,c=11},{op="SETVAR",var_name="w"},{op="GETVAR",var_name="x"},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="y"},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="z"},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="w"},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="x"},{op="LOADK",reg=17,k=1},{op="CMP",cmp_type="GT"},{op="JMPZ",offset=3},{op="LOADK",reg=18,k=8},{op="CALL",func="print",arg_count=1},{op="JMP",offset=2},{op="LOADK",reg=19,k=9},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=20,k=8},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=21,k=9},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="x"},{op="LOADK",reg=22,k=10},{op="CMP",cmp_type="EQ"},{op="JMPZ",offset=3},{op="LOADK",reg=23,k=11},{op="CALL",func="print",arg_count=1},{op="JMP",offset=2},{op="LOADK",reg=24,k=12},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=25,k=11},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=26,k=12},{op="CALL",func="print",arg_count=1},{op="GETVAR",var_name="x"},{op="LOADK",reg=27,k=3},{op="CMP",cmp_type="LT"},{op="JMPZ",offset=3},{op="LOADK",reg=28,k=13},{op="CALL",func="print",arg_count=1},{op="JMP",offset=2},{op="LOADK",reg=29,k=14},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=30,k=13},{op="CALL",func="print",arg_count=1},{op="LOADK",reg=31,k=14},{op="CALL",func="print",arg_count=1}}
 vm:run()
