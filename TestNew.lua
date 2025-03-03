0c0a45977bf01e4c61599c33d707b303a0fa3b1b
HEXHUB <Neliogaultier@gmail.com>
2025-03-03 22:55:55 +0100

    Update TestNew.lua

--- TestNew.lua
+++ TestNew.lua
@@ -1,10 +1,25 @@
 local vm={stack={}}
 local output_log=""
+local type=type or function()return"unknown"end
 local print=type(print)=="function" and print or function()end
+function vm:log(msg)
+    print("Logging: type of print is "..type(print))
+    print(msg)
+    output_log=output_log..msg.."
+"
+end
 function vm:run()
-    print("LuaRPH-like VM Starting")
+    print("Before loop: type of print is "..type(print))
+    self:log("LuaRPH-like VM Starting")
+    print("After first log: type of print is "..type(print))
+    while self.ip<=#self.code do
+        print("Inside loop: type of print is "..type(print))
+        local inst=self.code[self.ip]
+        self:log("Executing: "..inst.op.." at ip: "..self.ip)
+        self.ip=self.ip+1
+    end
 end
 vm.constants={}
-vm.code={}
+vm.code={{op="NOP"}}
 
 vm:run()
