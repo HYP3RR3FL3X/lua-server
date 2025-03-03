a960e291b489de20d159435865d96dc957d3103e
HEXHUB <Neliogaultier@gmail.com>
2025-03-03 22:13:53 +0100

    Update TestNew.lua

--- TestNew.lua
+++ TestNew.lua
@@ -2,8 +2,10 @@ local vm={stack={},instructions={},ip=1,vars={}}
 local output_log=""
 local identityFunc=function()end
 identityFunc("Wave Executor running")
-function vm:log(msg)print(msg)output_log=output_log..msg.."\n"end
+function vm:log(msg)print(msg)output_log=output_log..msg.."
+"end
 function vm:run()while self.ip<=#self.instructions do self.ip=self.ip+1 end end
 vm.constants={}
 vm.code={}
+
 vm:run()
